3f7eb73d933d4398b8e68c6d754c453dc9ce0f4e
jjadcruzeirodosul-hue <jjadcruzeirodosul@gmail.com>
2026-07-20 22:55:48 -0300

    Configura conexão inicial com Firebase

---
+++ app.js
@@ -0,0 +1,4 @@
+import { db } from "./js/firebase.js";
+
+console.log("Firebase inicializado com sucesso.");
+console.log("Instância do Firestore:", db);
---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB166r_T5-8aDzDuy8gi8MVSRARovVfH1o",
+  authDomain: "adcs-presenca-jiu-jitsu.firebaseapp.com",
+  projectId: "adcs-presenca-jiu-jitsu",
+  storageBucket: "adcs-presenca-jiu-jitsu.firebasestorage.app",
+  messagingSenderId: "49159692549",
+  appId: "1:49159692549:web:19df22d3a556f0aaaad76c"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export { firebaseApp, db };
