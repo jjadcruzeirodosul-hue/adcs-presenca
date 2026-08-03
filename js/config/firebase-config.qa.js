8db8c7968ba4d33a9f0e609a3d2e80491748cafd
jjadcruzeirodosul-hue <jjadcruzeirodosul@gmail.com>
2026-08-02 21:31:18 -0300

    feat: implementa configuração Firebase multiambiente

---
+++ js/config/firebase-config.qa.js
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDz4OLHFFTp5651bvQ_-68e5WAHCg-Y_lk",
+  authDomain: "adcs-presenca-qa.firebaseapp.com",
+  projectId: "adcs-presenca-qa",
+  storageBucket: "adcs-presenca-qa.firebasestorage.app",
+  messagingSenderId: "167818041291",
+  appId: "1:167818041291:web:e45993a9cd15735673484e",
+  measurementId: "G-041BMW044Y"
+};
+
+export const firebaseEnvironment = {
+  name: "QA",
+  projectId: "adcs-presenca-qa"
+};
--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB166r_T5-8aDzDuy8gi8MVSRARovVfH1o",
-  authDomain: "adcs-presenca-jiu-jitsu.firebaseapp.com",
-  projectId: "adcs-presenca-jiu-jitsu",
-  storageBucket: "adcs-presenca-jiu-jitsu.firebasestorage.app",
-  messagingSenderId: "49159692549",
-  appId: "1:49159692549:web:19df22d3a556f0aaaad76c"
-};
+import {
+    firebaseConfig,
+    firebaseEnvironment
+} from "./firebase-config.js";
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
+
+console.info(
+    `[Firebase] Ambiente: ${firebaseEnvironment.name} | Projeto: ${firebaseEnvironment.projectId}`
+);
 
 export { firebaseApp, db };
