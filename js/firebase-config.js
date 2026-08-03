8aecfbe110f5cecd2bf7ba6bca0484337462262f
jjadcruzeirodosul-hue <jjadcruzeirodosul@gmail.com>
2026-08-02 21:51:58 -0300

    feat: implementa infraestrutura Firebase multiambiente (#1)
    * chore: configura aliases Firebase para DEV QA e PROD
    
    * chore: restringe artefatos publicados no Firebase Hosting
    
    * feat: implementa configuração Firebase multiambiente

---
+++ js/firebase-config.js
@@ -0,0 +1,39 @@
+const CONFIG_BY_HOSTNAME = {
+  "localhost": "./config/firebase-config.dev.js",
+  "127.0.0.1": "./config/firebase-config.dev.js",
+
+  "adcs-presenca-jiu-jitsu.web.app":
+    "./config/firebase-config.dev.js",
+  "adcs-presenca-jiu-jitsu.firebaseapp.com":
+    "./config/firebase-config.dev.js",
+
+  "adcs-presenca-qa.web.app":
+    "./config/firebase-config.qa.js",
+  "adcs-presenca-qa.firebaseapp.com":
+    "./config/firebase-config.qa.js",
+
+  "adcs-presenca-prod.web.app":
+    "./config/firebase-config.prod.js",
+  "adcs-presenca-prod.firebaseapp.com":
+    "./config/firebase-config.prod.js"
+};
+
+const hostname = window.location.hostname;
+
+const configModulePath = CONFIG_BY_HOSTNAME[hostname];
+
+if (!configModulePath) {
+  throw new Error(
+    `[Firebase] Ambiente não reconhecido para o hostname: ${hostname}`
+  );
+}
+
+const {
+  firebaseConfig,
+  firebaseEnvironment
+} = await import(configModulePath);
+
+export {
+  firebaseConfig,
+  firebaseEnvironment
+};
